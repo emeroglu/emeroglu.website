b7d17b039c577e2ceea9edd937e364d602ef34ae
Erhan Emre Eroğlu <emeroglu@ku.edu.tr>
2019-03-24 01:18:51 +0300

    initial frontend

---
+++ emeroglu/emeroglu.com/Files/init.js
@@ -0,0 +1,88 @@
+﻿String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+String.prototype.capitalize = function () {
+
+    var initial = this.charAt(0).toUpperCase();
+    var rest = this.slice(1);
+    var restLower = "";
+
+    for (var i = 0; i < rest.length; i++) {
+        restLower += rest[i].toLowerCase();
+    }
+
+    return initial + restLower;
+
+};
+
+Date.prototype.truncate = function () {
+
+    let day = this.getDate();
+    let month = this.getMonth() + 1;
+    let year = this.getFullYear();
+
+    if (month < 10)
+        month = "0" + month;
+
+    return year + "-" + month + "-" + day + " 12:00:00";
+
+};
+
+Date.prototype.shorten = function () {
+
+    let months = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"];
+    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];    
+
+    let day = this.getDate();
+    let month = months[this.getMonth()];
+    let dow = days[this.getDay()];
+
+    if (month < 10)
+        month = "0" + month;
+
+    return day + " " + month + " " + dow;
+
+};
+
+$window.onresize = function () {
+
+    let w = $window.innerWidth;
+    let port = $view.port;
+
+    if (1200 <= w)
+        $view.port = "wide";
+    else if (992 <= w)
+        $view.port = "medium";
+    else if (768 <= w)
+        $view.port = "narrow";
+    else if (500 <= w)
+        $view.port = "tablet"
+    else
+        $view.port = "mobile"
+
+    if ($view.ports.includes($view.port)) {
+
+    } else {
+        $view.ports.push($view.port);
+        $bcast.shout("viewport_new");
+    }
+
+    if (port != $view.port)
+        $bcast.shout("viewport_changed");
+
+    $bcast.shout("viewport_" + $view.port);
+
+};
+
+$global.$fetch = new $fetch();
+
+$fetch
+    .begin()
+        .addStylesheet("/File/Style/" + version)
+        .addScript("/File/Script/" + version)
+        .onFetch(function () {                                 
+
+        })
+    .start();
---
+++ emeroglu/emeroglu.com/Files/js/tools/fetch.js
@@ -0,0 +1,77 @@
+﻿$js.compile("$fetch", null, function ($public, $private, $protected, $self) {
+
+    $private.field.index = -1;
+    $private.field.queue = [];
+
+    $public.delegate.begin = function () { $self.index = -1; $self.queue = []; return $self; };
+
+    $public.delegate.addStylesheet = function (_url) { $self.queue.push({ type: "stylesheet", url: _url }); return $self };
+    $public.delegate.addScript = function (_url) { $self.queue.push({ type: "script", url: _url }); return $self };
+
+    $private.void.on_fetch = function () { };
+    $public.delegate.onFetch = function ($delegate) { $self.on_fetch = $delegate; return $self; }
+
+    $private.void.on_recurse_end = function () { };
+    $private.void.recurse = function () {
+
+        $self.index++;
+
+        if ($self.index == $self.queue.length) {
+
+            $self.on_recurse_end();
+
+            return;
+
+        } else {
+
+            let item = $self.queue[$self.index];
+
+            eval("$self." + item.type + "(item.url, $self.recurse);");
+
+        }
+
+    };
+
+    $private.void.stylesheet = function (_url, $on_load) {
+
+        let e = document.createElement("link");
+        e.setAttribute("rel", "stylesheet");
+        e.setAttribute("href", _url);
+        e.onload = $on_load;
+
+        document.head.appendChild(e);
+
+    };
+
+    $private.void.script = function (_url, $on_load) {
+
+        let e = document.createElement("script");
+        e.setAttribute("type", "text/javascript");
+        e.setAttribute("src", _url);
+        e.onload = function () {
+
+            this.remove();
+
+            $on_load();
+
+        };
+
+        document.body.appendChild(e);
+
+    };
+
+    $public.void.module = function (_name, $on_load) {
+
+        let url = "/File/Module/" + version + "/" + _name;
+        $self.script(url, $on_load);
+
+    };
+
+    $public.void.start = function () {
+
+        $self.on_recurse_end = $self.on_fetch;
+        $self.recurse();
+
+    };
+
+});
